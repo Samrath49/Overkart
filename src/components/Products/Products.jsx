cdfaa270028f2bbf753e4e5eab742d6565dd4a24
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-02 16:23:40 +0530

    CommerceJS implementation

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from "@material-ui/core";
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: "Jacket", price: '$800', description: "Leather jacket", image: "https://images.unsplash.com/photo-1551028719-00167b16eac5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80" },
@@ -8,8 +9,10 @@ const products = [
 ]
 
 const Products = () => {
+    const style = useStyles();
     return (
-        <main>
+        <main className={style.content}>
+            <div className={style.toolbar} />
             <Grid container justify='center' spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.key} xs={12} sm={6} md={4} lg={3}>
