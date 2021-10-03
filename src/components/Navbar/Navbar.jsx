a0cb9e2dfcc43c0a96fa35feaf7cb4661ca22909
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-03 22:00:41 +0530

    Imporved Routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/overkart2.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useStyle from './styles';
 
 const Navbar = ({ totalItems }) => {
     const style = useStyle();
+    const location = useLocation();
+
     return (
         <>
             <AppBar position="fixed" className={style.appBar} color="inherit">
@@ -16,13 +18,14 @@ const Navbar = ({ totalItems }) => {
                         Overkart
                     </Typography>
                     <div className={style.grow} />
-                    <div className={style.button}>
-                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
-                            <Badge badgeContent={totalItems} color="secondary">
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                    </div>
+                    {location.pathname === '/' && (
+                        <div className={style.button}>
+                            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                                <Badge badgeContent={totalItems} color="secondary">
+                                    <ShoppingCart />
+                                </Badge>
+                            </IconButton>
+                        </div>)}
                 </Toolbar>
             </AppBar>
         </>
