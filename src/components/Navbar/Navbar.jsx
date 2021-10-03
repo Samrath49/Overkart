e88cf0ca6fd50f8f9542019cba409be42f3997f8
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-03 21:52:50 +0530

    Router Done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/overkart2.png';
+import { Link } from 'react-router-dom';
 import useStyle from './styles';
 
 const Navbar = ({ totalItems }) => {
@@ -10,13 +11,13 @@ const Navbar = ({ totalItems }) => {
         <>
             <AppBar position="fixed" className={style.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={style.title} color="inherit">
+                    <Typography component={Link} to="/  " variant="h6" className={style.title} color="inherit">
                         <img src={logo} alt="Overkart" height="45px" className={style.image} />
                         Overkart
                     </Typography>
                     <div className={style.grow} />
                     <div className={style.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
