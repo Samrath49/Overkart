e2c7f630fbb30303cd572caba4292a3734f3e6fe
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-04 11:58:03 +0530

    Checkout Update

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import useStyles from './styles';
+const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
+    const style = useStyles();
+    const [activeStep, setActiveStep] = useState(0);
+
     return (
-        <h1>
-            Booking Done!🔥
-        </h1>
+        <>
+            <div className={style.toolbar} />
+            <main className={style.layout}>
+                <Paper className={style.paper}>
+                    <Typography variant="h4" align="center">Checkout</Typography>
+                    <Stepper activeStep={activeStep} className={style.stepper}>
+                        {steps.map((step) => {
+                            <Step key={step}>
+                                <StepLabel>{step}</StepLabel>
+                            </Step>
+                        })}
+                    </Stepper>
+                </Paper>
+            </main>
+        </>
     )
 }
 
