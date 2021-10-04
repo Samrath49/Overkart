4a3e7ba30f195ab769b7ea757a3cbad9d4c66919
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-04 10:41:24 +0530

    Making cart changes

--- src/app.js
+++ src/app.js
@@ -58,7 +58,11 @@ const App = () => {
                     </Route>
 
                     <Route exact path="/cart">
-                        <Cart cart={cart} />
+                        <Cart cart={cart}
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                        />
                     </Route>
                 </Switch>
             </div>
