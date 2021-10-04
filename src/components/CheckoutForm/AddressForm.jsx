9cc71d11ea827efe9c504c7cedb12f0fba8b2705
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-04 18:23:14 +0530

    Error Fix

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
-// import { Link } from 'react-router-dom';
 import { commerce } from '../../lib/commerce';
 import FormInput from './CustomTextField';
 
--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { TextField, Grid } from '@material-ui/core';
+import { TextField, Grid, Select } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form';
 
 const FormInput = ({ name, label, required }) => {
     const { control } = useFormContext();
+    const isError = false;
 
     return (
         <Grid item xs={12} sm={6}>
-            <Controller as={TextField} control={control} fullWidth name={name} label={label} required={required} />
+            <Controller
+                control={control}
+                name={name}
+                render={({ textField }) => (
+                    <TextField
+                        fullWidth
+                        label={label}
+                        required
+                    />
+                )}
+            />
         </Grid>
     )
 }
