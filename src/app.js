e9456296fb4a31ffadba3108f722f9c8c007ef7f
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-05 22:49:04 +0530

    Cart state changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const App = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -39,6 +41,24 @@ const App = () => {
         const { cart } = await commerce.cart.empty();
 
         setCart(cart);
+    }
+
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh();
+
+        setCart(newCart);
+    }
+
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+        try {
+            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+            setOrder(incomingOrder);
+            refreshCart()
+
+        } catch (error) {
+            setErrorMessage(error.data.error.message);
+        }
     }
 
     useEffect(() => {
