27ce0756dbad0d02c7c11ff22634b5f3234ea96b
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-06 12:22:27 +0530

    Modified Confirmation

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -16,6 +16,7 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
     const [shippingData, setShippingData] = useState({});
+    const [isFinshed, setIsFinshed] = useState(false);
     useEffect(() => {
         const generateToken = async () => {
             try {
@@ -41,6 +42,12 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     const Form = () => activeStep === 0
         ? <AddressForm checkoutToken={checkoutToken} next={next} />
         : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep} onCaptureCheckout={onCaptureCheckout} nextStep={nextStep} />
+
+    const timeout = () => {
+        setTimeout(() => {
+            console.log('Hello World!');
+        }, 4321);
+    }
 
     let Confirmation = () => order.customer ? (
         <>
