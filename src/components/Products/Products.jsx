bb04a08c203845a0ced62b0efd1e35eeb089496c
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2021-12-06 18:22:23 +0530

    Fixed: Typo

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 import { Grid } from "@material-ui/core";
-import Product from './Product/Product';
-import useStyles from './styles';
+import Product from "./Product/Product";
+import useStyles from "./styles";
 
 const Products = ({ products, onAddToCart }) => {
-    const style = useStyles();
-    return (
-        <main className={style.content}>
-            <div className={style.toolbar} />
-            <Grid container justify='center' spacing={4}>
-                {products.map((product) => (
-                    <Grid item key={product.key} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} onAddToCart={onAddToCart} />
-                    </Grid>
-                ))}
-            </Grid>
-        </main>
-    )
-}
+  const style = useStyles();
+  return (
+    <main className={style.content}>
+      <div className={style.toolbar} />
+      <Grid container justifyContent="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.key} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
 
-export default Products
+export default Products;
